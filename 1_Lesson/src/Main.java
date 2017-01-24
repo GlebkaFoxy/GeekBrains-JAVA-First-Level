87067ec35cc633753588b2c705bb6a4ad2bf61e0
Gleb Bezginov <gleb.bezginov@gmail.com>
2017-01-24 12:34:35 +0300

    Полность выполнено 1 задание по первому уроку, первой части.

--- 1_Lesson/src/Main.java
+++ 1_Lesson/src/Main.java
@@ -13,17 +13,63 @@
 public class Main {
     public static void main(String[] args) {
         // Примитивные переменные
+		
+		//Целочисленные
         // byte, память 8 бит, значения от -128 до 127
-        byte a = 1;
+        byte Byte = 1;
 
-        // short
-        short b = 2;
+        // short, память 16 бит, значения от -32768 до 32767
+        short Short = 2;
+		
+		// int, память 32 бит, значения от -2147483648 до 2147483647  
+        int Int = 3;
 
-        int c = 3;
-
-        long g = 4;
-
-
+		//long, память 64 бит, значения от -9223372036854775808 до 9223372036854775807
+        long Long = 4;
+		
+		//Дробные
+		//float, память 32 бита.  
+		float Float = 5.0f;
+		
+		//double, память 64 бита.
+		double Double = 6.0d;
+		
+		//char, память 16 бит, хранится в формате юникод
+		char Char =7;
+		
+		// Принимает значение true или false
+		boolean Bolean = true; 
+		
+		System.out.println("Ответ третьего задания: "+e);
+		System.out.println();
+		doFourthTask();
+		System.out.println();
+		doFifhtTask();
 
     }
+	
+	public static doThirdTask(){
+		byte a,b,c,d= 1,2,8,4;
+		byte e = a*(b+(c/d));
+		return e;
+	}
+	
+	public static doFourthTask(){
+		byte First,Second = 7,6;
+		 boolean Ansver = true;
+		 
+		 if(First+Second<=10&&First+Second>=20){return Ansver;}
+		 else{
+			 Ansver=false;
+			 return Ansver;}
+	}
+	
+	public static doFifhtTask(){
+		int FirstYear=1704;
+		if(FirstYear %4==0){
+			if(FirstYear%100!=0||FirstYear%400==0)
+				System.out.println("Год является високосным")
+		}
+		else{System.out.println("Год не является високосным")}
+	}
 }
