66e2aacec221c6e3d87916bd36d8a20fd5fbe8ba
Gleb Bezginov <gleb.bezginov@gmail.com>
2017-01-31 18:01:50 +0300

    5 из 6 заданий выполнено, остался мини калькулятор.

--- 2_Lesson/src/Main2.java
+++ 2_Lesson/src/Main2.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by Глеб on 25.01.2017.
  *
@@ -11,6 +14,7 @@
 public class Main2 {
     public static void main(String[] args) {
         int[] Arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+        System.out.println("Задание 1 и 2");
         System.out.println("Массив Arr, имеет значения: ");
         for (int iArr=0; iArr<Arr.length; iArr++){
             System.out.print(Arr[iArr]+" ");
@@ -30,14 +34,38 @@ public class Main2 {
         }
         System.out.println();
         System.out.println("Задание 3, новый массив");
-        int[] Mass2 =new int[8];
-        for(int i=0;i<Mass2.length;i++){
-            Mass2[i]=i*3+1;
-            System.out.print(Mass2[i]+" ");
+        int[] Mass3 =new int[8];
+        for(int i=0;i<Mass3.length;i++){
+            Mass3[i]=i*3+1;
+            System.out.print(Mass3[i]+" ");
         }
-        int[] Mass3 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
-        for(int i=0; i<Mass3.length; i++){
+        int[] Mass4 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        System.out.println();
+        System.out.println("Задание 4");
+        for(int i=0; i<Mass4.length; i++){
+            if (Mass4[i]<6){Mass4[i]=Mass4[i]*2;}
+            System.out.print(Mass4[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Задание 5");
+        System.out.println("Массив имеет значения: ");
+        int[] Mass5 = {1,6,5,8,4,12,2,9,4,2,3,10};
+        int iMax=0;
+        int iMin=50;
+        for (int i=0; i<Mass5.length; i++){
+            if (Mass5[i]>iMax){iMax=Mass5[i];}
+            if (Mass5[i]<iMin){iMin=Mass5[i];}
+            System.out.print(Mass5[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Максимальное значение массива = "+iMax);
+        System.out.println("Минимальное значение массива = "+iMin);
+        System.out.println("Задание 6, консольный калькулятор");
 
-        }
+        System.out.println("Введите первое значение");
+        BufferedReader br =new BufferedReader(InputStreamReader(System.in));
+        int First = Integer.par
+        System.out.println("Введите знак *,/,+,-");
+        System.out.println("Введите второе значение");
     }
 }
